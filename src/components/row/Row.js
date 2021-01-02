778e479993c29ae68ec7910ed884b30c4ed47f9e
Teebreezy101 <tammy.batubo@gmail.com>
2021-01-02 22:50:31 +0100

    set up create react app and add row component

---
+++ src/components/row/Row.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './row.css';
+
+const Row = () => {
+    return (
+        <div className='row'>
+            I am the row
+        </div>
+    )
+}
+
+export default Row
