2ba32289379851bbfbb600976c10a65715e94abf
Teebreezy101 <tammy.batubo@gmail.com>
2021-01-03 02:18:13 +0100

    add function to format time for api and add styling to row component with dummy data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Row from './components/row/Row';
+import formatDate from './utils/formateDate';
 
 function App() {
+  const date = formatDate();
+  
+
   return (
-    <div className="App">
-      Hello world!
-    </div>
+  <div className="app">
+    <h1 className='app__header'>Most starred repos created in the last 30 days.</h1>
+    <Row />
+    <Row />
+  </div>
   );
 }
 
--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -4,7 +4,23 @@ import './row.css';
 const Row = () => {
     return (
         <div className='row'>
-            I am the row
+            <div className="row__image">
+                <img src="https://avatars0.githubusercontent.com/u/56544801?s=460&u=09ea785d2f540bff7f342ea533be4459c18aa12f&v=4" alt="avatar"/>
+            </div>
+
+            <div className="row__content">
+                <h2 className="row__name">Tensorflow</h2>
+                <p className="row__description">
+                    An Open Source Machine Learning Framework For Everyone https://tensorflow.org
+                </p>
+                <div className="row__details">
+                    <div className="row__stars"><p>Stars: <span>118k</span></p></div>
+                    <div className="row__issues">
+                        <p>Issues: <span>1.6k</span></p>
+                    </div>
+                    <p className="row__user">Submitted 30 days ago by <span className='row__username'>tensorflow</span></p>
+                </div>
+            </div>
         </div>
     )
 }
