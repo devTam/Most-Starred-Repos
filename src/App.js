a535f2d124876b8796828b59ab414829c89111ab
Teebreezy101 <tammy.batubo@gmail.com>
2021-01-05 02:07:52 +0100

    add api-call function and populate row components with data from api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import Row from './components/row/Row';
+import API_CALL from './utils/apiCall';
 import formatDate from './utils/formateDate';
 
 function App() {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
   const date = formatDate();
-  
+  const url = `https://api.github.com/search/repositories?q=created:>${date}&sort=stars&order=desc&page=${page}`;
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await API_CALL(url);
+      setData(data);
+    };
+    fetchData();
+  }, [url]);
+  
   return (
-  <div className="app">
-    <h1 className='app__header'>Most starred repos created in the last 30 days.</h1>
-    <Row />
-    <Row />
-  </div>
+    <div className="app">
+      <h1 className="app__header">
+        Most starred repos created in the last 30 days.
+      </h1>
+      <ul className="repos">
+        {data &&
+          data.map((item) => (
+            <li className="repo" key={item.id}>
+              <Row item={item} />
+            </li>
+          ))}
+      </ul>
+    </div>
   );
 }
 
--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import './row.css';
 
-const Row = () => {
+const Row = (props) => {
+    const {item: {name, description, stargazers_count, open_issues_count, owner} } = props;
+
     return (
         <div className='row'>
             <div className="row__image">
-                <img src="https://avatars0.githubusercontent.com/u/56544801?s=460&u=09ea785d2f540bff7f342ea533be4459c18aa12f&v=4" alt="avatar"/>
+                <img src={owner?.avatar_url} alt="avatar"/>
             </div>
 
             <div className="row__content">
-                <h2 className="row__name">Tensorflow</h2>
+                <h2 className="row__name">{name}</h2>
                 <p className="row__description">
-                    An Open Source Machine Learning Framework For Everyone https://tensorflow.org
+                    {description}
                 </p>
                 <div className="row__details">
                     <div className="stars-issues-container">
-                    <div className="row__stars"><p>Stars: <span>118k</span></p></div>
+                    <div className="row__stars"><p>Stars: <span>{stargazers_count}</span></p></div>
                     <div className="row__issues">
-                        <p>Issues: <span>1.6k</span></p>
+                        <p>Issues: <span>{open_issues_count}</span></p>
                     </div>
 
                     </div>
-                    <p className="row__user">Submitted 30 days ago by <span className='row__username'>tensorflow</span></p>
+                    <p className="row__user">Submitted 30 days ago by <span className='row__username'>{owner?.login}</span></p>
                 </div>
             </div>
         </div>
